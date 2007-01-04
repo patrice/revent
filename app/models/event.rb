2c1679a1d8b4fe1e1ecf6d38a74f0d78b020d8ad
Seth Walker <seth@radicaldesigns.org>
2007-01-04 02:10:40 +0000

    add foreign key to avoid deprecation warning
    git-svn-id: https://svn.radicaldesigns.org/daysofaction@31 8f85c81b-0f21-0410-93f9-e2b37b76a6fe

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,6 @@
 class Event < ActiveRecord::Base
   belongs_to :event_group
-  belongs_to :host, :class_name => 'User'
+  belongs_to :host, :class_name => 'User', :foreign_key => 'host_id'
   has_many :reports, :order => 'position'
   has_many :rsvps
   has_many :attendees, :through => 'rsvps', :source => :person
