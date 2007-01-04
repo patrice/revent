14e500b8fe726343dfb9858e4b7992080836536f
Seth Walker <seth@radicaldesigns.org>
2007-01-04 01:28:51 +0000

    add zip code search plugin
    git-svn-id: https://svn.radicaldesigns.org/daysofaction@27 8f85c81b-0f21-0410-93f9-e2b37b76a6fe

--- db/schema.rb
+++ db/schema.rb
@@ -2,68 +2,77 @@
 # migrations feature of ActiveRecord to incrementally modify your database, and
 # then regenerate this schema definition.
 
-ActiveRecord::Schema.define(:version => 5) do
+ActiveRecord::Schema.define(:version => 6) do
 
   create_table "event_groups", :force => true do |t|
-    t.column "name", :string
+    t.column "name",              :string
     t.column "short_description", :text
-    t.column "user_id", :integer
+    t.column "user_id",           :integer
   end
 
   create_table "events", :force => true do |t|
-    t.column "name", :string
+    t.column "name",              :string
     t.column "short_description", :text
-    t.column "description", :text
-    t.column "event_group_id", :integer
-    t.column "location", :text
-    t.column "city", :string
-    t.column "state", :string
-    t.column "postal_code", :string
-    t.column "host_id", :integer
-    t.column "start", :datetime
-    t.column "end", :datetime
+    t.column "description",       :text
+    t.column "event_group_id",    :integer
+    t.column "location",          :text
+    t.column "city",              :string
+    t.column "state",             :string
+    t.column "postal_code",       :string
+    t.column "host_id",           :integer
+    t.column "start",             :datetime
+    t.column "end",               :datetime
   end
 
   create_table "medias", :force => true do |t|
     t.column "content_type", :string
-    t.column "filename", :string
-    t.column "size", :integer
-    t.column "parent_id", :integer
-    t.column "thumbnail", :string
-    t.column "width", :integer
-    t.column "height", :integer
-    t.column "caption", :string
-    t.column "url", :string
-    t.column "position", :integer
-    t.column "author", :string
-    t.column "type", :string
-    t.column "user_id", :integer
-    t.column "event_id", :integer
+    t.column "filename",     :string
+    t.column "size",         :integer
+    t.column "parent_id",    :integer
+    t.column "thumbnail",    :string
+    t.column "width",        :integer
+    t.column "height",       :integer
+    t.column "caption",      :string
+    t.column "url",          :string
+    t.column "position",     :integer
+    t.column "author",       :string
+    t.column "type",         :string
+    t.column "user_id",      :integer
+    t.column "event_id",     :integer
   end
 
   create_table "reports", :force => true do |t|
     t.column "event_id", :integer
-    t.column "user_id", :integer
-    t.column "text", :text
+    t.column "user_id",  :integer
+    t.column "text",     :text
     t.column "position", :integer
   end
 
   create_table "rsvps", :id => false, :force => true do |t|
     t.column "event_id", :integer
-    t.column "user_id", :integer
-    t.column "comment", :text
-    t.column "guests", :integer
+    t.column "user_id",  :integer
+    t.column "comment",  :text
+    t.column "guests",   :integer
   end
 
   create_table "users", :force => true do |t|
-    t.column "login", :string
-    t.column "email", :string
-    t.column "crypted_password", :string, :limit => 40
-    t.column "salt", :string, :limit => 40
-    t.column "created_at", :datetime
-    t.column "updated_at", :datetime
-    t.column "remember_token", :string
+    t.column "login",                     :string
+    t.column "email",                     :string
+    t.column "crypted_password",          :string,   :limit => 40
+    t.column "salt",                      :string,   :limit => 40
+    t.column "created_at",                :datetime
+    t.column "updated_at",                :datetime
+    t.column "remember_token",            :string
     t.column "remember_token_expires_at", :datetime
+  end
+
+  create_table "zip_codes", :force => true do |t|
+    t.column "zip",       :string
+    t.column "city",      :string
+    t.column "state",     :string, :limit => 2
+    t.column "latitude",  :string
+    t.column "longitude", :string
+    t.column "zip_class", :string
   end
 
 end
