d756baa7f02098b66f474027944a31ed7664cc05
Seth Walker <seth@radicaldesigns.org>
2007-01-04 02:19:00 +0000

    add state onclick handler
    git-svn-id: https://svn.radicaldesigns.org/daysofaction@33 8f85c81b-0f21-0410-93f9-e2b37b76a6fe

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,2 +1,23 @@
 module EventsHelper
+  def gmaps_state_click(map)
+    html = []
+    html << "<script type=\"text/javascript\">\n/* <![CDATA[ */\n"  
+#    html << "GEvent.addListener(#{map.dom_id}, \"click\", function() { alert(\"You clicked the map.\"); });"
+    html << "/* ]]> */</script> "
+    return @debug ? html.join("\n") : html.join.gsub(/\s+/, ' ')
+  end
+
+  def gmaps_auto_center_and_zoom(map)
+=begin
+    html = []
+    html << "<script type=\"text/javascript\">\n/* <![CDATA[ */\n"  
+    html << " var bounds = new GLatLngBounds();
+    html << "/* ]]> */</script> "
+    return @debug ? html.join("\n") : html.join.gsub(/\s+/, ' ')
+=end
+  end
+
+  def state_centers
+    EventsController::STATE_CENTERS
+  end
 end
