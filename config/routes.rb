21083ebb4ee95f8c1ec4d7a0f71c2ac186a9aca2
Seth Walker <seth@radicaldesigns.org>
2007-01-10 20:36:43 +0000

    new default route
    git-svn-id: https://svn.radicaldesigns.org/daysofaction@118 8f85c81b-0f21-0410-93f9-e2b37b76a6fe

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ ActionController::Routing::Routes.draw do |map|
   # You can have the root of your site routed by hooking up '' 
   # -- just remember to delete public/index.html.
   # map.connect '', :controller => "welcome"
-  map.home '', :controller => "events"
+  map.home '', :controller => "reports"
   map.zip_search "events/search/:zip",  :controller => "events",
                                         :action => "by_zip",
                                         :requirements => { :zip => /\d{5}/ }
