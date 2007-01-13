980eaf89b90eb7d22c7c7943f5ee80f166fbc550
Seth Walker <seth@radicaldesigns.org>
2007-01-13 00:30:22 +0000

    exception notification doesn't like missing images
    git-svn-id: https://svn.radicaldesigns.org/daysofaction@146 8f85c81b-0f21-0410-93f9-e2b37b76a6fe

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -2,7 +2,7 @@
 # Likewise, all the methods added will be available for all controllers.
 class ApplicationController < ActionController::Base
   include AuthenticatedSystem
-  include ExceptionNotifiable
+#  include ExceptionNotifiable
   before_filter :login_from_cookie
   session :session_key => '_daysofaction_session_id'
   session :off, :if => Proc.new { |req| !(true == req.parameters[:admin]) }
