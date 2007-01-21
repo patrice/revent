9a2aa662c29d119e9b43783010170eca03566946
Seth Walker <seth@radicaldesigns.org>
2007-01-21 05:57:25 +0000

    dia load from Calendar model for cron goodness google geocode plugin i forgot from last time
    git-svn-id: https://svn.radicaldesigns.org/daysofaction@179 8f85c81b-0f21-0410-93f9-e2b37b76a6fe

--- trunk/app/controllers/site_controller.rb
+++ trunk/app/controllers/site_controller.rb
@@ -6,54 +6,14 @@ class SiteController < ApplicationController
     case params[:id]
     when 'test'
       require 'active_record/fixtures'
-        ['calendars.yml', 'events.yml', 'reports.yml'].each do |fixture_file| Fixtures.create_fixtures(File.join('test','fixtures'), File.basename(fixture_file, '.*'))
+      ['calendars.yml', 'events.yml', 'reports.yml'].each do |fixture_file| 
+        Fixtures.create_fixtures(File.join('test','fixtures'), File.basename(fixture_file, '.*'))
       end
+      flash[:notice] = "Test fixtures loaded"
     when 'dia'
-      opts = YAML.load_file(File.join(RAILS_ROOT,'config','democracyinaction-config.yml'))
-      require 'DIA_API_Simple'
-      api = DIA_API_Simple.new opts
-      events = api.get('event')
-      flash[:notice] = events.length.to_s + " events added from DIA"
-
-      gmaps = Cartographer::Header.new
-      if gmaps.has_key? request.env["HTTP_HOST"] #+ uri
-        application_id = gmaps.value_for request.env["HTTP_HOST"] #+ uri
-        require 'google_geocode'
-        gg = GoogleGeocode::Accuracy.new application_id
-      else
-        raise 'wtf'
-      end
-
-      unknown = inaccurate = 0
-      events.each do |e|
-        my_event = Event.find_or_create_by_service_foreign_key(e['event_KEY'])
-        my_event.calendar_id=1
-        my_event.name = e['Event_Name']
-        my_event.description = e['Description']
-        my_event.location = e['Address']
-        my_event.city = e['City']
-        my_event.state = e['State']
-        my_event.postal_code = e['Zip']
-        my_event.start = e['Start']
-        my_event.end = e['End']
-        my_event.directions = e['Directions']
-
-        begin
-          location = gg.locate my_event.address_for_geocode
-          if location.accuracy.to_i < 8
-            inaccurate = inaccurate + 1
-          else
-            my_event.latitude = location.latitude
-            my_event.longitude = location.longitude
-          end
-        rescue GoogleGeocode::AddressError => error
-          unknown = unknown + 1
-        end
-
-        my_event.save!
-      end
+      result = Calendar.load_from_dia(1, :host => request.env["HTTP_HOST"])
+      flash[:notice] = "#{result.imported} events imported, #{result.imported - result.unknown - result.inaccurate} geocoded; of the rest, #{result.unknown} google could not geocode, #{result.inaccurate} geocoded with too low a degree of accuracy"
     end
-    flash[:notice] = "#{events.length} events imported, #{events.length - unknown - inaccurate} geocoded; of the rest, #{unknown} google could not geocode, #{inaccurate} geocoded with too low a degree of accuracy"
     redirect_to '/admin/events'
   end
 end
--- trunk/app/models/calendar.rb
+++ trunk/app/models/calendar.rb
@@ -9,4 +9,54 @@ class Calendar < ActiveRecord::Base
   def national_footer_text
     'national footer text'
   end
+
+  DiaLoadResult = Struct.new(:imported, :unknown, :inaccurate)
+  def self.load_from_dia(id, *args)
+    cal = find(id)
+    return unless cal
+    options = args.last.is_a?(Hash) ? args.pop : {}
+    opts = YAML.load_file(File.join(RAILS_ROOT,'config','democracyinaction-config.yml'))
+    require 'DIA_API_Simple'
+    api = DIA_API_Simple.new opts
+    events = api.get('event')
+    gmaps = Cartographer::Header.new
+    if gmaps.has_key? options[:host]
+      application_id = gmaps.value_for options[:host]
+      require 'google_geocode'
+      gg = GoogleGeocode::Accuracy.new application_id
+    else
+      raise 'wtf'
+    end
+
+    result = DiaLoadResult.new 0, 0, 0
+    events.each do |e|
+      my_event = Event.find_or_create_by_service_foreign_key(e['event_KEY'])
+      my_event.calendar_id = id
+      my_event.name = e['Event_Name']
+      my_event.description = e['Description']
+      my_event.location = e['Address']
+      my_event.city = e['City']
+      my_event.state = e['State']
+      my_event.postal_code = e['Zip']
+      my_event.start = e['Start']
+      my_event.end = e['End']
+      my_event.directions = e['Directions']
+
+      begin
+        location = gg.locate my_event.address_for_geocode
+        if location.accuracy.to_i < 8
+          result.inaccurate = result.inaccurate + 1
+        else
+          my_event.latitude = location.latitude
+          my_event.longitude = location.longitude
+        end
+      rescue GoogleGeocode::AddressError => error
+        result.unknown = result.unknown + 1
+      end
+
+      my_event.save!
+    end
+    result.imported = events.length
+    return result
+  end
 end
