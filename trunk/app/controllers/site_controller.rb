7377f7262c1e33bc460bd6f29e2becc46f847103
Seth Walker <seth@radicaldesigns.org>
2007-03-27 05:07:16 +0000

    first pass dynamic image map
    git-svn-id: https://svn.radicaldesigns.org/daysofaction@303 8f85c81b-0f21-0410-93f9-e2b37b76a6fe

--- trunk/app/controllers/site_controller.rb
+++ trunk/app/controllers/site_controller.rb
@@ -1,6 +1,35 @@
 class SiteController < ApplicationController
-  before_filter :login_required
-  access_control :DEFAULT => 'admin'
+  before_filter :login_required, :only => :load_data
+  access_control :load_data => 'admin'
+
+  def map
+    require 'RMagick'
+    image = Magick::Image.read(File.join(RAILS_ROOT,'lib','fresh.png')).first
+    width = image.columns
+    height = image.rows
+    gc = Magick::Draw.new
+    gc.fill "#75AD50"
+    events = Event.find(:all)
+    coords = events.collect {|e| e.national_map_coordinates}.select do |c|
+      c && c[0].nonzero? && c[1].nonzero? &&
+        (24.520833..49.384472).include?(c[0]) &&
+        (-124.736611..-66.950556).include?(c[1])
+    end
+    gxf = 8.9
+    gyf = 11.55
+    gxs = 56.4
+    gys = 23.9
+    coords.each do |c|
+      y     = (height - (-gys + c[0]) * gyf).round
+      rat   = (height - y) / height
+      gxfa  = gxf - (rat * 1.13)
+      gxsa  = gxs - (rat * 5.77)
+      x     = (width + ((gxsa + c[1])*gxfa)).round
+      gc.circle x+5, y-5, x+10, y-5
+    end
+    gc.draw image
+    send_data image.to_blob, :type => image.mime_type, :disposition => 'inline'
+  end
 
   def load_data
     case params[:id]
