05fa062a8ee1cd8f722288250c5e4d42f6476440
Seth Walker <seth@radicaldesigns.org>
2007-04-14 21:34:43 +0000

    don't expire flashmap do expire lightboxen
    git-svn-id: https://svn.radicaldesigns.org/daysofaction@471 8f85c81b-0f21-0410-93f9-e2b37b76a6fe

--- trunk/app/models/report_sweeper.rb
+++ trunk/app/models/report_sweeper.rb
@@ -6,11 +6,12 @@ class ReportSweeper < ActionController::Caching::Sweeper
     expire_page :controller => "/events", :action => "show", :id => record.event
     expire_page :controller => "/reports", :action => "list"
     expire_page :controller => "/reports", :action => "search", :state => record.event.state
-    expire_page :controller => "/reports", :action => "flashmap", :format => 'xml'
+#    expire_page :controller => "/reports", :action => "flashmap", :format => 'xml'
     FileUtils.rm_rf(File.join(ActionController::Base.page_cache_directory,'reports','page')) rescue Errno::ENOENT
 
     expire_page :controller => "/reports", :action => "press" unless record.press_links.empty?
     expire_page :controller => "/reports", :action => "video" if record.embed?
+    record.attachments.each {|a| expire_page :controller => "/reports", :action => "lightbox", :id => a.id}
   end
 
   alias after_destroy after_save
