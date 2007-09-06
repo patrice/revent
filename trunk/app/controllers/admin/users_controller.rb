32aae96460ef274a21ff721f422f564d74e39955
Patrice de Muizon <patrice@radicaldesigns.org>
2007-09-06 00:20:13 +0000

    Should have been part of previous check-in.  Allow admin to reset password for all users of a site.
    git-svn-id: https://svn.radicaldesigns.org/daysofaction@591 8f85c81b-0f21-0410-93f9-e2b37b76a6fe

--- trunk/app/controllers/admin/users_controller.rb
+++ trunk/app/controllers/admin/users_controller.rb
@@ -1,2 +1,27 @@
 class Admin::UsersController < AdminController
+  def index
+    @user_pages, @users = paginate(:users, :conditions => ['site_id = ?', Site.current.id], :order => 'last_name', :per_page => 10)
+  end
+  
+  def search
+    @user = User.find_by_email(params[:email])
+    if @user.nil?
+      flash.now[:notice] = "Could not find a user with that email address"
+      index && render(:action => 'index') and return
+    end
+  end
+  
+  def reset_password
+    @user = User.find(params[:id])
+    if request.post?      
+      @user.password = params[:user][:password] if params[:user][:password]
+      @user.password_confirmation = params[:user][:password_confirmation] if params[:user][:password_confirmation]
+      if (@user.save)
+        flash.now[:notice] = "Password reset for " + @user.full_name
+        index and render(:action => 'index') and return
+      else
+        flash.now[:notice] = "Password not reset for " + @user.full_name
+      end      
+    end
+  end
 end
