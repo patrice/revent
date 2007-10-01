8e9799f4ce3485f0a4c62af6bd87a09060161ae9
Patrice de Muizon <patrice@radicaldesigns.org>
2007-10-01 16:57:21 +0000

    Began implementing admin/invites features.  Partially working.  Will deploy so that Jon & company have something to play with.
    git-svn-id: https://svn.radicaldesigns.org/daysofaction@734 8f85c81b-0f21-0410-93f9-e2b37b76a6fe

--- trunk/app/controllers/admin/invites_controller.rb
+++ trunk/app/controllers/admin/invites_controller.rb
@@ -1,23 +1,29 @@
 class Admin::InvitesController < AdminController
   def index
-    @politicians = Politician.find(:all, :include => [:rsvps, :politician_invites], :limit => 400)
-    @events = @calendar.events.find(:all, :select => "name")
+    @politicians = Politician.find(:all, :include => [:rsvps, :politician_invites], :limit => 10)
+    @events = @calendar.events.find(:all, :select => "name, id")
+    @select_events = []
+    @events.each {|e| @select_events << [e.name, e.id]}
+    
   end
   
   def edit
     @politician = Politician.find(params[:id])
-    if request.post?
-      @politician.update_attributes(params[:politician])
-    end
-  end
-  
-  def invite
-    p = Politician.find(params[:id])
+    @politician.update_attributes(params[:politician]) if request.post?
   end
   
   def confirm
     @politician = Politician.find(params[:id])
-    
+    r = @politician.rsvps.build(:event_id => params[:event])
+    r.save
+    index and render :action => 'index'
+  end
+  
+  def unconfirm
+    @politician = Politician.find(params[:id])
+    @rsvp = Rsvp.find(params[:rsvp])
+    @politician.rsvps.delete(@rsvp)
+    index and render :action => 'index'
   end
   
 end
