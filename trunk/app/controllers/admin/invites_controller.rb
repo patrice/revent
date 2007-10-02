402c816082e52973b4beb700f1db5f6d7f5f7288
Patrice de Muizon <patrice@radicaldesigns.org>
2007-10-02 14:48:01 +0000

    Allow for editing of politician contact info.
    git-svn-id: https://svn.radicaldesigns.org/daysofaction@748 8f85c81b-0f21-0410-93f9-e2b37b76a6fe

--- trunk/app/controllers/admin/invites_controller.rb
+++ trunk/app/controllers/admin/invites_controller.rb
@@ -4,26 +4,28 @@ class Admin::InvitesController < AdminController
     @events = @calendar.events.find(:all, :select => "name, id", :order => "name")
     @select_events = []
     @events.each {|e| @select_events << [e.name, e.id]}
-    
   end
   
   def edit
     @politician = Politician.find(params[:id])
-    @politician.update_attributes(params[:politician]) if request.post?
+    if request.post?
+      @politician.update_attributes(params[:politician]) 
+      redirect_to :action => 'index'
+    end
   end
   
   def confirm
     @politician = Politician.find(params[:id])
     r = @politician.rsvps.build(:event_id => params[:event])
     r.save
-    index and render :action => 'index'
+    redirect_to :action => 'index'
   end
   
   def unconfirm
     @politician = Politician.find(params[:id])
     @rsvp = Rsvp.find(params[:rsvp])
     @politician.rsvps.delete(@rsvp)
-    index and render :action => 'index'
+    redirect_to :action => 'index'
   end
   
 end
