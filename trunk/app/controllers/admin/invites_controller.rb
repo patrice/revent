8bbe3e45e1619461bba2bcd28c50f04c4222a8c3
Patrice de Muizon <patrice@radicaldesigns.org>
2007-10-04 16:34:11 +0000

    Enabled editing of politician contact info.  Added searching capability.
    git-svn-id: https://svn.radicaldesigns.org/daysofaction@763 8f85c81b-0f21-0410-93f9-e2b37b76a6fe

--- trunk/app/controllers/admin/invites_controller.rb
+++ trunk/app/controllers/admin/invites_controller.rb
@@ -1,15 +1,49 @@
-class Admin::InvitesController < AdminController
+class Admin::InvitesController < AdminController 
+ 
+  def all
+    @politicians = Politician.find(:all, :include => [:rsvps, :politician_invites]) 
+    @select_events = @calendar.events.find(:all).map{|e| [e.name, e.id]}
+    render :action => :index
+  end
+
   def index
-    @politicians = Politician.find(:all, :include => [:rsvps, :politician_invites])
+    @politicians = Candidate.find(:all, :include => [:rsvps, :politician_invites], :conditions => ['office = ?', 'president'])
+    @select_events = @calendar.events.find(:all).map{|e| [e.name, e.id]}
+  end
+  
+  def search
+    fields = [] 
+    values = []
+    fields << "(first_name like ? OR last_name like ? OR display_name like ?)"
+    3.times {values << params[:name] + "%"}
+    fields << "state like ?" 
+    values << params[:state].downcase + "%" 
+    query = [fields.join(' AND '), values].flatten
+    @politicians = Politician.find(:all, :conditions => query, :order => "district_type DESC")
     @events = @calendar.events.find(:all, :select => "name, id", :order => "name")
     @select_events = []
     @events.each {|e| @select_events << [e.name, e.id]}
-  end
-  
+    render :action => 'index'
+  end 
+
+  def new 
+    @candidate = Candidate.new(params[:candidate])
+    if request.post?
+      @candidate.display_name = @candidate.first_name + ' ' + @candidate.last_name
+      @candidate.save
+      flash[:notice] = 'New candidate has been added!'
+      redirect_to :action => 'index'
+    end
+  end 
+
   def edit
     @politician = Politician.find(params[:id])
     if request.post?
-      @politician.update_attributes(params[:politician]) 
+      p = @politician
+      p.display_name.gsub!(p.first_name, params[:politician][:first_name]) if params[:politician][:first_name]
+      p.display_name.gsub!(p.last_name, params[:politician][:last_name]) if params[:politician][:last_name]
+      p.update_attributes(params[:politician]) 
+      flash[:notice] = 'Politician information has been updated!'
       redirect_to :action => 'index'
     end
   end
@@ -27,5 +61,5 @@ class Admin::InvitesController < AdminController
     @politician.rsvps.delete(@rsvp)
     redirect_to :action => 'index'
   end
-  
 end
+
