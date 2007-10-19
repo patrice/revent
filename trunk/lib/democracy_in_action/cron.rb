0cb84f4cc98ea28cc62ea914ff0efd69bd71df12
Seth Walker <seth@radicaldesigns.org>
2007-10-19 23:54:27 +0000

    also import invitations by recipient key
    git-svn-id: https://svn.radicaldesigns.org/daysofaction@823 8f85c81b-0f21-0410-93f9-e2b37b76a6fe

--- trunk/lib/democracy_in_action/cron.rb
+++ trunk/lib/democracy_in_action/cron.rb
@@ -10,26 +10,39 @@ module DemocracyInAction
       api = DemocracyInAction::API.new(@config)
       supporter_campaigns = api.get 'supporter_campaign', 'where' => "supporter_campaign.Date_Created > '#{ago.to_s(:db)}'"
       supporter_campaigns.collect do |sc|
-        user = User.find_or_initialize_by_site_id_and_email Site.current.id, sc['Email']
-        unless user.crypted_password || (user.password && user.password_confirmation)
-          password = Digest::SHA1.hexdigest( Time.now.to_s.split(//).sort_by {rand}.join )
-          user.password = user.password_confirmation = password
-        end 
-        user.first_name = sc['First_Name']
-        user.last_name = sc['Last_Name']
-        user.save
-        politicians_ids = sc['person_legislator_IDS'].split(',')
-        event = DemocracyInActionObject.find_by_table_and_key('campaign', sc['campaign_KEY']).associated
-        next unless event
-        politicians_ids.collect do |p|
-          politician = Politician.find_by_person_legislator_id(p)
-          next unless politician
-          invite = PoliticianInvite.find_or_initialize_by_politician_id_and_event_id_and_user_id_and_invite_type(politician.id, event.id, user.id, 'email')
-          next unless invite.new_record?
-          invite.build_democracy_in_action_object :table => 'supporter_campaign', :key => sc['key'], :local => sc
-          invite
-        end
-      end.flatten.compact.collect {|i| i.save}
+        import_sc(sc)
+      end
+    end
+
+    def import_sc(sc)
+      user = User.find_or_initialize_by_site_id_and_email Site.current.id, sc['Email']
+      unless user.crypted_password || (user.password && user.password_confirmation)
+        password = Digest::SHA1.hexdigest( Time.now.to_s.split(//).sort_by {rand}.join )
+        user.password = user.password_confirmation = password
+      end 
+      user.first_name = sc['First_Name']
+      user.last_name = sc['Last_Name']
+      user.save
+      politicians_ids = sc['person_legislator_IDS'].split(',').map(&:strip)
+      recipient_keys = sc['recipient_KEYS'].split(',').map(&:strip)
+      event = DemocracyInActionObject.find_by_table_and_key('campaign', sc['campaign_KEY']).associated
+      next unless event
+      politicians_ids.each do |p|
+        politician = Politician.find_by_person_legislator_id(p)
+        next unless politician
+        invite = PoliticianInvite.find_or_initialize_by_politician_id_and_event_id_and_user_id_and_invite_type(politician.id, event.id, user.id, 'email')
+        next unless invite.new_record?
+        invite.build_democracy_in_action_object :table => 'supporter_campaign', :key => sc['key'], :local => sc
+        invite.save
+      end
+      recipient_keys.each do |k|
+        politician = Politician.find :first, :include => :democracy_in_action_object, :conditions => "democracy_in_action_objects.key = #{k}"
+        next unless politician
+        invite = PoliticianInvite.find_or_initialize_by_politician_id_and_event_id_and_user_id_and_invite_type(politician.id, event.id, user.id, 'email')
+        next unless invite.new_record?
+        invite.build_democracy_in_action_object :table => 'supporter_campaign', :key => sc['key'], :local => sc
+        invite.save
+      end
     end
   end
 end
