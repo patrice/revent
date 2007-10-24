4d645011a6b735898b591e8b7429ecca419c43d2
Patrice de Muizon <patrice@radicaldesigns.org>
2007-10-24 21:53:55 +0000

    Added ability to export all events with organizer contact info.
    git-svn-id: https://svn.radicaldesigns.org/daysofaction@844 8f85c81b-0f21-0410-93f9-e2b37b76a6fe

--- trunk/app/controllers/admin/events_controller.rb
+++ trunk/app/controllers/admin/events_controller.rb
@@ -33,4 +33,16 @@ class Admin::EventsController < AdminController
     @events = @calendar.events.find(:all, :conditions => "latitude = 0 or latitude IS NULL or longitude = 0 or longitude IS NULL")
     render :action => 'search'
   end
+  
+  def export
+    @events = @calendar.events.find(:all, :include => :host)
+    require 'fastercsv'
+    string = FasterCSV.generate do |csv|
+      csv << ["Event Name", "City", "State", "Postal_Code", "Organizer", "Email", "Phone"]
+      @events.each do |event|
+        csv << [event.name, event.city, event.state, event.postal_code, event.host.full_name, event.host.email, event.host.phone]
+      end
+    end
+    send_data(string, :type => 'text/csv; charset=utf-8; header=present', :filename => "events.csv")
+  end
 end
