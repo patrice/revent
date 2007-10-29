2521e080745348e3ed5ef23a1e46049a27d9da60
Seth Walker <seth@radicaldesigns.org>
2007-10-29 18:18:07 +0000

    partners can render their own page
    git-svn-id: https://svn.radicaldesigns.org/daysofaction@865 8f85c81b-0f21-0410-93f9-e2b37b76a6fe

--- trunk/app/controllers/partners_controller.rb
+++ trunk/app/controllers/partners_controller.rb
@@ -1,8 +1,18 @@
 class PartnersController < ApplicationController
   skip_before_filter :calendar
   skip_before_filter :set_cache_root
-  def index
+
+  before_filter :set_partner_cookie
+  def set_partner_cookie
     cookies[:partner] = {:value => params[:id], :expires => 3.hours.from_now} if params[:id]
-    redirect_to home_url
+  end
+  caches_action :index
+  def index
+    file = File.join(RAILS_ROOT, 'themes', current_theme, 'views', 'partners', "#{params[:id]}.rhtml")
+    if(File.exists?(file))
+      render :file => file, :layout => true
+    else
+      redirect_to home_url
+    end
   end
 end
