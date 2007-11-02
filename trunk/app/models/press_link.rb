d9c60aba33cddba477f38730a459b979743f42b4
Patrice de Muizon <patrice@radicaldesigns.org>
2007-11-02 01:19:43 +0000

    Replaced auto_link with link_to and added validation to press_link to make sure url's work with link_to.
    git-svn-id: https://svn.radicaldesigns.org/daysofaction@894 8f85c81b-0f21-0410-93f9-e2b37b76a6fe

--- trunk/app/models/press_link.rb
+++ trunk/app/models/press_link.rb
@@ -1,3 +1,19 @@
 class PressLink < ActiveRecord::Base
   belongs_to :report
+  
+  def validate 
+    begin
+      URI.parse(self.url)
+    rescue URI::InvalidURIError
+      errors.add(:url, 'The format of the URL is not valid.')
+    end
+  end
+  
+  def before_save
+    # add 'http://' at beginning of url if not there
+    if not self.url =~ /^https?:\/\/.*/
+      self.url = "http://" + self.url
+    end
+  end
 end
+
