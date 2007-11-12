1264233e3e37eb966f9b7b023479dbaa8dee9a42
David Taylor <david@radicaldesigns.org>
2007-11-12 14:17:58 +0000

    abstracted reports and rsvp forms even more
    git-svn-id: https://svn.radicaldesigns.org/daysofaction@968 8f85c81b-0f21-0410-93f9-e2b37b76a6fe

--- trunk/app/controllers/admin/calendars2_controller.rb
+++ trunk/app/controllers/admin/calendars2_controller.rb
@@ -4,7 +4,7 @@ class Admin::Calendars2Controller < AdminController
 	end
 
   active_scaffold :calendar do |config|
-  	config.columns = [:name, :permalink, :short_description, :event_start, :event_end, :signup_redirect, :hostform, :current, :theme, :letter_script, :call_script, :site ]
+  	config.columns = [:name, :permalink, :short_description, :event_start, :event_end, :signup_redirect, :hostform, :rsvp_dia_group_key, :rsvp_dia_trigger_key, :rsvp_redirect,  :report_title_text, :report_intro_text, :report_dia_group_key, :report_dia_trigger_key, :report_redirect, :flickr_tag, :flickr_additional_tags, :flickr_photoset, :current, :theme, :letter_script, :call_script,  :site ]
   	config.list.columns = [:name, :permalink, :site]
   	config.list.sorting = [{ :name => :asc}]
   	columns[:site].ui_type = :select
