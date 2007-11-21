91d73080615a0b5f0d2a82c48c75b243fe686055
Patrice de Muizon <patrice@radicaldesigns.org>
2007-11-21 21:35:17 +0000

    Added printing status to long migrate.
    git-svn-id: https://svn.radicaldesigns.org/daysofaction@998 8f85c81b-0f21-0410-93f9-e2b37b76a6fe

--- trunk/db/migrate/069_fallback_lat_lng.rb
+++ trunk/db/migrate/069_fallback_lat_lng.rb
@@ -2,9 +2,11 @@ class FallbackLatLng < ActiveRecord::Migration
   def self.up
     add_column :events, :fallback_latitude,  :float
     add_column :events, :fallback_longitude, :float
-    
+
+    puts 'Processing all events...'
     Event.find(:all).each do |e|
       # event gets re-geocoded on save
+      puts '.'
       e.save unless e.latitude and e.longitude 
     end
   end
