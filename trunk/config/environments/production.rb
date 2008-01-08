c0c6cd4df8e221c1d7953cd66a964ee6667e7fe1
Patrice de Muizon <patrice@radicaldesigns.org>
2008-01-08 18:53:49 +0000

    add sweepers to observers list.
    git-svn-id: https://svn.radicaldesigns.org/daysofaction@1106 8f85c81b-0f21-0410-93f9-e2b37b76a6fe

--- trunk/config/environments/production.rb
+++ trunk/config/environments/production.rb
@@ -14,7 +14,7 @@ config.logger.level = Logger::INFO
 # Full error reports are disabled and caching is turned on
 config.action_controller.consider_all_requests_local = false
 config.action_controller.perform_caching             = true
-config.active_record.observers = :event_sweeper, :politician_invite_sweeper
+config.active_record.observers = :event_sweeper, :politician_invite_sweeper, :rsvp_sweeper, :calendar_sweeper
 
 config.active_record.verification_timeout = 14400
 
