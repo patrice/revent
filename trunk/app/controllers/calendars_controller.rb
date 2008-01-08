cb885f74c727e179268cc05b74653bcb1a01dfd5
Patrice de Muizon <patrice@radicaldesigns.org>
2008-01-08 22:46:35 +0000

    remove some of these unused calendar methods that may be a security risk.
    git-svn-id: https://svn.radicaldesigns.org/daysofaction@1113 8f85c81b-0f21-0410-93f9-e2b37b76a6fe

--- trunk/app/controllers/calendars_controller.rb
+++ trunk/app/controllers/calendars_controller.rb
@@ -1,11 +1,6 @@
 class CalendarsController < ApplicationController
   caches_page :show
 
-  def index
-    list
-    render :action => 'list'
-  end
-
   def signup
     # same action post vs get
     if File.exists?(File.join(Theme.path_to_theme(@site.theme), 'views', 'calendars', @calendar.id.to_s, 'signup.liquid'))
@@ -21,10 +16,6 @@ class CalendarsController < ApplicationController
   # GETs should be safe (see http://www.w3.org/2001/tag/doc/whenToUseGet.html)
   verify :method => :post, :only => [ :create ],
          :redirect_to => { :action => :list }
-
-  def list
-    @calendars = Site.current.calendars
-  end
 
   def events
     @events = @calendar.events
