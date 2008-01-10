75ea5301885d96b99466127e8194c9e4498423b2
Patrice de Muizon <patrice@radicaldesigns.org>
2008-01-10 06:50:26 +0000

    fix admin hostforms editing so that site gets set automatically and calendar drop-down comes up with current value.
    git-svn-id: https://svn.radicaldesigns.org/daysofaction@1126 8f85c81b-0f21-0410-93f9-e2b37b76a6fe

--- trunk/app/helpers/admin/hostforms_helper.rb
+++ trunk/app/helpers/admin/hostforms_helper.rb
@@ -1,11 +1,5 @@
 module Admin::HostformsHelper
-
-def site_id_form_column( record, input_name )
-  return Site.find( record.site_id ).host if record.site_id
-  record.site_id = Site.current.id
-  text_field( :record, :site_id )
-end
-def calendar_form_column( record, input_name )
-  select_tag input_name, options_for_select(Site.current.calendars.map{ |c| [c.name, c.id] })
-end
+  def calendar_form_column(calendar, input_name)
+    select_tag(input_name, options_for_select(Site.current.calendars.map{|c| [c.name, c.id]}, calendar.id))
+  end
 end
