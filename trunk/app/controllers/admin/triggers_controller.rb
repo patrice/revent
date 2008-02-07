81abe6cf2b131310cb662385f274bc420ebd0f5b
Patrice de Muizon <patrice@radicaldesigns.org>
2008-02-07 01:18:02 +0000

    allow editing of html emails
    git-svn-id: https://svn.radicaldesigns.org/daysofaction@1301 8f85c81b-0f21-0410-93f9-e2b37b76a6fe

--- trunk/app/controllers/admin/triggers_controller.rb
+++ trunk/app/controllers/admin/triggers_controller.rb
@@ -4,7 +4,7 @@ class Admin::TriggersController < AdminController
 
 	active_scaffold :trigger do |config|
     config.label = "Email Response Triggers"
-  	config.columns = [:name, :calendar, :site, :from_name, :from, :reply_to, :bcc, :subject, :email_text]
+  	config.columns = [:name, :calendar, :site, :from_name, :from, :reply_to, :bcc, :subject, :email_text, :email_html]
   	config.columns[:calendar].form_ui = :select     # just want drop down on form (no crazy subforms) 	
   	config.columns[:calendar].clear_link            # just want calendar name on list (no links)
 #  	config.columns[:calendar].description = "Calendar triggers over-ride 'All Calendars' triggers"
@@ -12,7 +12,8 @@ class Admin::TriggersController < AdminController
   	config.columns[:name].label = "Email Trigger"
   	config.columns[:from].label = "From Email Address"
   	config.columns[:from_name].label = "From Name"
-  	config.columns[:email_text].label = "Message (Plain Text)<br /><br />Use the following tokens to insert event information (must include square-backets):<br /><br />[EVENT_NAME], [EVENT_ADDRESS], [EVENT_CITY], [EVENT_STATE], [EVENT_START_DATE], [EVENT_START_TIME], [EVENT_LINK], [SIGNUP_LINK], [MANAGE_LINK], [REPORT_LINK], [NEW_REPORT_LINK], [HOST_NAME], [HOST_PHONE], [HOST_EMAIL]"
+  	config.columns[:email_text].label = "Message (Plain Text)<br /><br />The following tokens are available to insert event information (must include square-backets):<br /><br />[EVENT_NAME], [EVENT_ADDRESS], [EVENT_CITY], [EVENT_STATE], [EVENT_START_DATE], [EVENT_START_TIME], [EVENT_LINK], [SIGNUP_LINK], [MANAGE_LINK], [REPORT_LINK], [NEW_REPORT_LINK], [HOST_NAME], [HOST_PHONE], [HOST_EMAIL]"
+  	config.columns[:email_html].label = "Message (HTML)<br /><br />All event information tokens listed above are also available here."
   	config.list.columns = [:name, :calendar, :subject, :from_name, :from]
   	config.list.sorting = [{:calendar => :asc}, {:name => :asc}]
   end
