602c1d97e6c79fc28da67e2eaeaf7bae1854f428
Patrice de Muizon <patrice@radicaldesigns.org>
2008-02-22 02:02:56 +0000

    disable sorting since it broke trigger admin.
    git-svn-id: https://svn.radicaldesigns.org/daysofaction@1354 8f85c81b-0f21-0410-93f9-e2b37b76a6fe

--- trunk/app/controllers/admin/hostforms_controller.rb
+++ trunk/app/controllers/admin/hostforms_controller.rb
@@ -8,7 +8,7 @@ class Admin::HostformsController < AdminController
     config.label = "Event Host Sign Up Forms"
   	config.columns = [:calendar, :title, :intro_text, :event_info_text,  :pre_submit_text, :thank_you_text,  :dia_trigger_key, :dia_group_key, :dia_user_tracking_code, :dia_event_tracking_code, :tag, :redirect]  	
   	config.list.columns = [:title, :calendar, :site]
-  	config.list.sorting = [{ :calendar => :asc}, {:title => :asc}]
+#  	config.list.sorting = [{ :calendar => :asc}, {:title => :asc}]
   #	columns[:calendar].ui_type = :select
 	#columns[:site].ui_type = :select
   end
