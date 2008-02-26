f162532ffa9f57ba912586eb2e631ad6fc33107f
Patrice de Muizon <patrice@radicaldesigns.org>
2008-02-26 17:18:17 +0000

    change tense of appended event info on host messages to attendees. added test for this
    git-svn-id: https://svn.radicaldesigns.org/daysofaction@1361 8f85c81b-0f21-0410-93f9-e2b37b76a6fe

--- trunk/test/unit/user_mailer_test.rb
+++ trunk/test/unit/user_mailer_test.rb
@@ -3,17 +3,29 @@ require File.dirname(__FILE__) + '/../test_helper'
 class UserMailerTest < Test::Unit::TestCase
 
   def setup
+    Site.current = Site.new(:host => 'events.radicaldesigns.org')  
     @user = User.new(:email => 'test@gmail.com')
-    @events = [Event.new(:id => 111)]
+    @events = [Event.new(:id => 111, :start => 1.day.ago, :end => (1.day.ago + 2.hours))]
     @calendar = Calendar.new(:id => 222, :admin_email => 'info@radicaldesigns.org')
     @user.stubs(:events).returns(@events)
     Event.any_instance.stubs(:calendar).returns(@calendar)
   end 
 
   def test_activation 
-    Site.current = Site.new(:host => 'events.radicaldesigns.org')  
     response = UserMailer.create_activation(@user)
     assert_equal('info@radicaldesigns.org', response.from[0])
   end
+  
+  def test_message
+    @from = User.new(:email => 'michealjordan@gmail.com')
+    @event = Event.new(:id => 333, :name => 'Step It Up Rally', :city => 'San Francisco', :state => 'CA', :location => '1370 Mission St., 4th Fl.', :start => 1.day.ago, :end => (1.day.ago + 2.hours))
+    @message = {:subject => 'check, check, testing, 1, 2, 3', :body => 'test upcoming/past events'}
+    response = UserMailer.create_message(@from, @event, @message)
+    assert_match(/took place on/, response.body)
 
+    @event.start = 1.day.ago
+    @event.end = 1.day.ago + 2.hours
+    response = UserMailer.create_message(@from, @event, @message)
+    assert_match(/will take place on/, response.body)
+  end
 end
