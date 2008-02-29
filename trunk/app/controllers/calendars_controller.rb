9fd0cf4e497070a9c3fc4286b07a56b2ee2e6b3e
Patrice de Muizon <patrice@radicaldesigns.org>
2008-02-29 16:04:18 +0000

    display number of usersfor energy action. remove unused CalendarsController::signup
    git-svn-id: https://svn.radicaldesigns.org/daysofaction@1373 8f85c81b-0f21-0410-93f9-e2b37b76a6fe

--- trunk/app/controllers/calendars_controller.rb
+++ trunk/app/controllers/calendars_controller.rb
@@ -1,18 +1,6 @@
 class CalendarsController < ApplicationController
   caches_page :show
 
-  def signup
-    # same action post vs get
-    if File.exists?(File.join(Theme.path_to_theme(@site.theme), 'views', 'calendars', @calendar.id.to_s, 'signup.liquid'))
-      @content = Liquid::Template.parse(File.read(File.join(Theme.path_to_theme(@site.theme), 'views', 'calendars', @calendar.id.to_s, 'signup.liquid'))).render
-      render :text => @content and return
-    end
-    render :file => "calendars/#{@calendar.id}/new" and return
-    #XXX: boo, this sucks, move this
-  rescue
-    render "events/new"
-  end
-
   # GETs should be safe (see http://www.w3.org/2001/tag/doc/whenToUseGet.html)
   verify :method => :post, :only => [ :create ],
          :redirect_to => { :action => :list }
@@ -20,5 +8,9 @@ class CalendarsController < ApplicationController
   def show
     # event query is in events_controller#flashmap or inline in themes/#{theme}/views/calendars/show.rhtml
     @flashmap_data_url = url_for(:permalink => @calendar.permalink, :controller => 'events', :action => 'flashmap', :only_path => true)
+  end
+  
+  def num_users
+    render :inline => Site.current.users.length.to_s
   end
 end
