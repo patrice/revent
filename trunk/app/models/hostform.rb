6161e1f0658009ee89521a5b9d3db6cc138087c9
Patrice de Muizon <patrice@radicaldesigns.org>
2008-03-12 18:13:44 +0000

    only validate hostform tag if it isnt blank.
    git-svn-id: https://svn.radicaldesigns.org/daysofaction@1417 8f85c81b-0f21-0410-93f9-e2b37b76a6fe

--- trunk/app/models/hostform.rb
+++ trunk/app/models/hostform.rb
@@ -3,5 +3,8 @@ class Hostform < ActiveRecord::Base
 	belongs_to :site
 	has_one :trigger
 	
-  validates_format_of :tag, :with => /^[a-zA-Z0-9\_\-]+$/, :message => "can not contain special characters"
+  validates_format_of :tag, :with => /^[a-zA-Z0-9\_\-]+$/, :message => "can not contain special characters", :if => :tag_not_blank
+  def tag_not_blank
+    not self.tag.blank?
+  end
 end
