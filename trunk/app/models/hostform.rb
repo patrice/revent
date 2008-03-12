8f05447365d3d16eee4ac8ae68c34d51529a8e14
Patrice de Muizon <patrice@radicaldesigns.org>
2008-03-12 01:49:42 +0000

    add validation for tag (to disallow spaces).
    git-svn-id: https://svn.radicaldesigns.org/daysofaction@1408 8f85c81b-0f21-0410-93f9-e2b37b76a6fe

--- trunk/app/models/hostform.rb
+++ trunk/app/models/hostform.rb
@@ -2,4 +2,6 @@ class Hostform < ActiveRecord::Base
 	belongs_to :calendar
 	belongs_to :site
 	has_one :trigger
+	
+  validates_format_of :tag, :with => /^[a-zA-Z0-9\_\-]+$/, :message => "can not contain special characters"
 end
