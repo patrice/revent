837bea95758d04112be6fa00dad9530388eaed25
Patrice de Muizon <patrice@radicaldesigns.org>
2008-03-18 22:20:36 +0000

    list 7
    git-svn-id: https://svn.radicaldesigns.org/daysofaction@1438 8f85c81b-0f21-0410-93f9-e2b37b76a6fe

--- trunk/app/controllers/reports_controller.rb
+++ trunk/app/controllers/reports_controller.rb
@@ -250,18 +250,9 @@ class ReportsController < ApplicationController
 
   def featured
     if params[:id]
-      @reports = @calendar.featured_reports.find(:all, :conditions => ["events.state = ?", params[:id]], :include => :event, :limit => 5, :order => 'reports.created_at DESC')
+      @reports = @calendar.featured_reports.find(:all, :conditions => ["events.state = ?", params[:id]], :include => :event, :limit => 7, :order => 'reports.created_at DESC')
     else
-      @reports = @calendar.featured_reports.find(:all, :include => :event, :limit => 5, :order => 'reports.created_at DESC')
-    end
-    render :layout => false
-  end
-
-  def featured_video
-    if params[:id]
-      @reports = @calendar.featured_reports.find(:all, :include => [:event, :embeds], :conditions => ["events.state = ?", params[:id]], :limit => 5, :order => 'reports.created_at DESC')
-    else
-      @reports = @calendar.featured_reports.find(:all, :include => [:event, :embeds], :conditions => 'embeds.id', :limit => 5, :order => 'reports.created_at DESC')
+      @reports = @calendar.featured_reports.find(:all, :include => :event, :limit => 7, :order => 'reports.created_at DESC')
     end
     render :layout => false
   end
