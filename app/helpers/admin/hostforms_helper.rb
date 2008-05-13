46939949c16c149e438de669340f42f926cc3c24
Patrice de Muizon <patrice@radicaldesigns.org>
2008-05-13 12:04:02 -0700

    fix calendar dropdown on admin hostform.

--- app/helpers/admin/hostforms_helper.rb
+++ app/helpers/admin/hostforms_helper.rb
@@ -1,5 +1,5 @@
 module Admin::HostformsHelper
-  def calendar_form_column(calendar, input_name)
-    select_tag(input_name, options_for_select(Site.current.sorted_calendars.map{|c| [c.name, c.id]}, calendar.id))
+  def calendar_form_column(hostform, input_name)
+    select_tag(input_name, options_for_select(Site.current.sorted_calendars.map{|c| [c.name, c.id]}, hostform.calendar.id))
   end
 end
