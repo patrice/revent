9e8ff60afa970a6247ddd4b246ce9d8bfb5ada46
Patrice de Muizon <patrice@radicaldesigns.org>
2008-05-21 17:00:20 -0700

    fix up host_reminder since its ready to run.

--- lib/host_reminder.rb
+++ lib/host_reminder.rb
@@ -1,7 +1,10 @@
-today = Time.now
-yesterday = today - 1.day
-events = Event.find(:all, :conditions => ["events.end > ? AND events.end <= ?", yesterday, today])
+events = Event.find(:all, :conditions => ["events.end <= ? AND events.end > ?", Time.now, 1.day.ago])
 events.each do |e|
-  trigger = e.calendar.triggers.find_by_name("Report Host Reminder") || e.calendar.site.triggers.find_by_name("Report Host Reminder")
+  trigger = nil
+  if e.calendar.triggers
+    trigger = e.calendar.triggers.find_by_name("Report Host Reminder") 
+  elsif
+    trigger = e.calendar.site.triggers.find_by_name("Report Host Reminder")
+  end
   TriggerMailer.deliver_trigger(trigger, e.host, e, e.calendar.site.host) if trigger
 end
