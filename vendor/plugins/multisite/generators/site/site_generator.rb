62f5c7c1a82d266b429d93b70659c64a19fe36c1
Patrice de Muizon <patrice@radicaldesigns.org>
2008-05-28 13:37:46 -0700

    change site generator to use theme.

--- vendor/plugins/multisite/generators/site/site_generator.rb
+++ vendor/plugins/multisite/generators/site/site_generator.rb
@@ -3,10 +3,10 @@ class SiteGenerator < Rails::Generator::NamedBase
    def manifest
       record do |m|
           # Site folder(s)
-          m.directory File.join( "sites", file_name )
+          m.directory File.join( "themes", file_name )
           # Site content folders
-          m.directory File.join( "sites", file_name, "views" )
-          m.directory File.join( "sites", file_name, "public" )
+          m.directory File.join( "themes", file_name, "views" )
+          m.directory File.join( "themes", file_name, "public" )
       end
    end
 end
