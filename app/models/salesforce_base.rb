b7df1dc284ee18787a7f6fa2d2060f0da022bd32
Patrice de Muizon <patrice@radicaldesigns.org>
2008-05-28 13:58:03 -0700

    remove connection from base, since this will happen in workling.

--- app/models/salesforce_base.rb
+++ app/models/salesforce_base.rb
@@ -1,7 +1,9 @@
 class SalesforceBase < ActiveRecord::Base
   self.abstract_class = true
+=begin
   salesforce_config = File.join(Site.current_config_path, 'salesforce-config.yml')
   if File.exists?(salesforce_config)
     self.establish_connection YAML.load_file(salesforce_config)
   end
+=end
 end
