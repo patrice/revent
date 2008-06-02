06640b348fa5248eef7390bdb07ec34e7ebddd47
Seth Walker <seth@radicaldesigns.org>
2008-06-02 16:08:57 -0700

    configure workling and vendor evented starling (defunkt version)

--- app/workers/salesforce_worker.rb
+++ app/workers/salesforce_worker.rb
@@ -1,30 +1,11 @@
 class SalesforceWorker < Workling::Base
-  def establish_connection
-    SalesforceBase.flush_connections
-    salesforce_config = File.join(Site.current_config_path, 'salesforce-config.yml')
-    if File.exists?(salesforce_config)
-      SalesforceBase.establish_connection YAML.load_file(salesforce_config)
-    end
+  def save_contact(options={})
+    RAILS_DEFAULT_LOGGER.debug "SalesforceWorker received user id: #{options[:user_id]}"
+    u = User.find(options[:user_id])
+#    SalesforceWorker.create_with_user(u)
   end
 
-  def sync_contacts
-    User.find(:all, :conditions => ["salesforce_sync = 1"], :group => "site_id").each do |user|
-      unless (Site.current == user.site_id)
-        Site.current = user.site_id and establish_connection
-      end
-      sfcontact = SalesforceContact.create(
-        :phone => user.phone,
-        :first_name => user.first_name,
-        :last_name => user.last_name,
-        :mailing_address => user.street,
-        :mailing_address2 => user.street2,
-        :mailing_state => user.state,
-        :mailing_country => user.country,
-        :mailing_postal_code => user.postal_code)
-      user.update_attribute(:salesforce_sync, false)
-    end
-  end
-
+=begin
   def sync_events
     Event.find(:all, :conditions => ["salesforce_sync = 1"], :group => "site_id").each do |event|
       sfevent = SalesforceEvent.create(
@@ -36,4 +17,5 @@ class SalesforceWorker < Workling::Base
 
     end
   end
+=end
 end
