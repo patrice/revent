db42577baf383e333d04b50a8abb4c1f193daaa1
Patrice de Muizon <patrice@radicaldesigns.org>
2008-06-16 16:04:13 -0700

    Add test for specifying new dia urls.

---
+++ spec/lib/democracy_in_action/config.rb
@@ -0,0 +1,11 @@
+require File.dirname(__FILE__) + '/../../spec_helper'
+
+describe DemocracyInAction::Config do
+  it "should read the DIA urls from the config file" do
+    @config = DemocracyInAction::Config.new(File.join(RAILS_ROOT,'test','config','democracyinaction-config1.yml'))
+    @config['urls'].should == 
+      { 'get'     => 'http://org2.democracyinaction.org/dia/api/get.jsp', 
+        'process' => 'http://org2.democracyinaction.org/dia/api/process.jsp',
+        'delete'  => 'http://org2.democracyinaction.org/dia/api/delete.jsp'}
+  end
+end
