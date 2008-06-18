25c8372782fdf3db137e1f877639455c4edfb90e
Patrice de Muizon <patrice@radicaldesigns.org>
2008-06-17 18:34:41 -0700

    Remove dia test config file.

--- spec/lib/democracy_in_action/config.rb
+++ spec/lib/democracy_in_action/config.rb
@@ -2,7 +2,7 @@ require File.dirname(__FILE__) + '/../../spec_helper'
 
 describe DemocracyInAction::Config do
   it "should read the DIA urls from the config file" do
-    @config = DemocracyInAction::Config.new(File.join(RAILS_ROOT,'test','config','democracyinaction-config1.yml'))
+    @config = DemocracyInAction::Config.new(File.join(RAILS_ROOT,'test','config','democracyinaction-config.yml'))
     @config['urls'].should == 
       { 'get'     => 'http://org2.democracyinaction.org/dia/api/get.jsp', 
         'process' => 'http://org2.democracyinaction.org/dia/api/process.jsp',
