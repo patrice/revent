59c04efb73b36f3cc0a831f30f5dba8e382f25e5
Seth Walker <seth@radicaldesigns.org>
2008-07-11 12:55:17 -0700

    start spec for report create

--- spec/models/report_spec.rb
+++ spec/models/report_spec.rb
@@ -1,25 +1,39 @@
 require File.dirname(__FILE__) + '/../spec_helper.rb'
 
-$starling_path = "tmp/starling"
-$starling_pid_file = File.join($starling_path, "starling.pid")
-
 describe Report do
-  before(:all) do
-#    `sudo starling -d -p 332233 --pid #{$starling_pid_file} -q #{$starling_path}`  
-  end
   describe "when created" do 
-    it "should upload attachments to flickr" do
-      
-      
+    before do
+      @site = create_site
+      @site.stub!(:salesforce_enabled?).and_return(false)
+      Site.stub!(:current).and_return(@site)
+      @host = @site.users.create( :first_name => 'hostesst', :email => 'jak@crack.com' )
+      @calendar = create_calendar(:site => @site)
+      @event = create_event( :calendar => @calendar, :host => @host, :country_code => 'GBR' )
     end
-  end
-  after(:all) do
-=begin
-    File.open($starling_pid_file, "r") do |f| 
-      pid = f.gets
-      `sudo kill -9 #{pid}` 
+
+    it "should should update existing users" do
+      @user = create_user :email => 'test@example.com', :site => @site
+      @report = create_report :reporter_data => {:first_name => 'testy', :last_name => 'mctest', :email => 'test@example.com'}, :event => @event, :user => nil
+#      User.find(@user).first_name.should == 'testy'
+      User.find(:first, :conditions => { :first_name => 'testy', :site_id => @site }).should == @report.user
     end
-    `sudo rm #{$starling_pid_file}`
-=end
+
+    it "should create new users" do
+      User.delete_all
+      @report = create_report :reporter_data => {:first_name => 'testy', :last_name => 'mctest', :email => 'test@example.com'}, :event => @event, :user => nil
+      User.find(:first, :conditions => { :first_name => 'testy', :site_id => @site }).should == @report.user
+    end
+
+    it "should create press links"
+
+    it "should create attachments"
+
+    it "should check akismet"
+
+    it "should save to flickr"
+
+    it "should tag attachments"
+
+    it "should create embeds"
   end
 end
