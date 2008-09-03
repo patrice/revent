c1e5c6457911a1e9860ae31286336a5f69ec31cf
Radical Designs <dev@radicaldesigns.org>
2008-09-03 15:22:04 -0700

    add custom attributes and event host/attending columns to csv export

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -41,12 +41,13 @@ class Admin::UsersController < AdminController
   end
 
   def export
-    @users = Site.current.users
+    @users = Site.current.users.find(:all, :include => :custom_attributes)
+    @attribute_names = @users.inject([]) {|names, u| names << u.custom_attributes.map {|a| a.name }; names.flatten.compact.uniq }
     require 'fastercsv'
     string = FasterCSV.generate do |csv|
-      csv << ["Email", "First_Name", "Last_Name", "Phone", "Street", "Street_2", "City", "State", "Postal_Code"]
+      csv << ["Email", "First_Name", "Last_Name", "Phone", "Street", "Street_2", "City", "State", "Postal_Code", "Events_Hosting_IDS", "Events_Attending_IDS"] + @attribute_names
       @users.each do |user|
-        csv << [user.email, user.first_name, user.last_name, user.phone, user.street, user.street_2, user.city, user.state, user.postal_code]
+        csv << [user.email, user.first_name, user.last_name, user.phone, user.street, user.street_2, user.city, user.state, user.postal_code] + [ user.event_ids.join(','), user.attending_ids.join(',') ] + @attribute_names.map {|a| user.custom_attributes_data.send(a.to_sym) }
       end
     end
     send_data(string, :type => 'text/csv; charset=utf-8; header=present', :filename => "#{Site.current.theme}_users.csv")
