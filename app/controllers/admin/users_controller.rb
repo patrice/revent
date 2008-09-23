87101a6fb4ab45f26232599f45857809571d747c
Patrice de Muizon <patrice@radicaldesigns.org>
2008-09-22 18:32:25 -0700

    Offload generation of user export to queue.

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -41,15 +41,18 @@ class Admin::UsersController < AdminController
   end
 
   def export
-    @users = Site.current.users.find(:all, :include => :custom_attributes)
-    @attribute_names = @users.inject([]) {|names, u| names << u.custom_attributes.map {|a| a.name }; names.flatten.compact.uniq }
-    require 'fastercsv'
-    string = FasterCSV.generate do |csv|
-      csv << ["Email", "First_Name", "Last_Name", "Phone", "Street", "Street_2", "City", "State", "Postal_Code", "Partner_Code", "Events_Hosting_IDS", "Events_Attending_IDS"] + @attribute_names
-      @users.each do |user|
-        csv << [user.email, user.first_name, user.last_name, user.phone, user.street, user.street_2, user.city, user.state, user.postal_code, user.partner_id] + [ user.event_ids.join(','), user.attending_ids.join(',') ] + @attribute_names.map {|a| user.custom_attributes_data.send(a.to_sym) }
-      end
+    @start = Time.now.strftime("%Y%m%d%H%M")
+    ExportWorker.async_export_users(Site.current.id, start)
+  end
+
+  def download_export
+    download_file = File.join(RAILS_ROOT, 'tmp', "#{Site.current.theme}_users_#{start}.csv")
+    unless File.exists?(
+      flash[:notice] = "Export not ready to download. Check back in a few minutes"
+      redirect_to(:action => 'export') && return
     end
-    send_data(string, :type => 'text/csv; charset=utf-8; header=present', :filename => "#{Site.current.theme}_users.csv")
+    File.new("#{Site.current.theme}_users.csv") do |f|
+      send_data(string, :type => 'text/csv; charset=utf-8; header=present', :filename => "#{Site.current.theme}_users.csv")
+    end
   end
 end
